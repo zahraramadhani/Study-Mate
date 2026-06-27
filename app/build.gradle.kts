cc613f1d23572c1b9605ea78364041ce0f8ce190
zahraramadhani <ramadhanisanjayazahra@gmail.com>
2026-06-27 10:44:31 +0700

    Konfigurasi tanda tangan rilis menggunakan keystore otomatis

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -20,9 +20,19 @@ android {
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
+    signingConfigs {
+        create("release") {
+            storeFile = file("keystore.jks")
+            storePassword = "studymate123"
+            keyAlias = "studymate"
+            keyPassword = "studymate123"
+        }
+    }
+
     buildTypes {
         release {
             isMinifyEnabled = false
+            signingConfig = signingConfigs.getByName("release")
             proguardFiles(
                 getDefaultProguardFile("proguard-android-optimize.txt"),
                 "proguard-rules.pro"
